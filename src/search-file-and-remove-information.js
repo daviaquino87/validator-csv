319d5c8a8cb293a1741194fa047d6a0fd46e8e89
daviaquino87 <davi26031@gmail.com@gmail.com>
2023-10-11 10:50:56 -0300

    refactor: correction on character scape function

--- src/search-file-and-remove-information.js
+++ src/search-file-and-remove-information.js
@@ -21,7 +21,7 @@ function searchFileAndRemoveInformation({ filePath, separator }) {
             const headers = newArray[0].split(separator);
             const rows = [];
 
-            newArray.forEach((element) => {
+            newArray.splice(0).forEach((element) => {
               let isEscape = false;
               let row = [];
               let currentItem = "";
