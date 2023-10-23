0f55024c967aba7ac5f299ee388487adb676b72f
daviaquino87 <davi26031@gmail.com@gmail.com>
2023-10-23 16:13:45 -0300

    refactor: new example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-const validateCSV = require("./csv-validator");
-const functionsValidate = require("./utils/functions-validate");
+const path = require("path");
+const validator = require("validator-csv");
+const ejs = require("ejs");
+const Yup = require("yup");
 
-const validator = {
-  validateCSV,
-  functionsValidate,
-};
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-module.exports = validator;
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+
+const filePath = path.resolve(__dirname, "..", "uploads", "customers.csv");
+
+const validationSchema = Yup.object().shape({
+  age: Yup.number().min(18, "Invalid age, the customer must be over 18 years old.").required("The field age is required."),
+});
+
+async function bootstrap() {
+  const data = await validator.validateCSV({
+    filePath: filePath,
+    headers: ["name", "age", "gender"],
+    schema: validationSchema,
+  });
+
+  const headers = data.headers;
+  const rows = data.rows.map(([name, age, gender, error]) => ({
+    name,
+    age,
+    gender,
+    error,
+  }));
+
+  app.get("/", (req, res) => {
+    res.render("index", { headers, rows });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`server running on PORT: ${PORT}`);
+  });
+}
+
+bootstrap();
